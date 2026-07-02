adb6d6378f70abd49d07e0fd6a11f0c6ea086088
khushikri1209 <khushikumari12029@gmail.com>
2026-07-02 21:53:22 +0530

    Create J03Anagram.java

---
+++ 03String/J03Anagram.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class J03Anagram {
+
+    public static boolean isAnagram(String s1, String s2) {
+
+        // Remove spaces and convert to lowercase
+        s1 = s1.replaceAll("\\s", "").toLowerCase();
+        s2 = s2.replaceAll("\\s", "").toLowerCase();
+
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+    }
+
+    public static void main(String[] args) {
+
+        String s1 = "Listen";
+        String s2 = "Silent";
+
+        if (isAnagram(s1, s2)) {
+            System.out.println("Anagram");
+        } else {
+            System.out.println("Not Anagram");
+        }
+    }
+}

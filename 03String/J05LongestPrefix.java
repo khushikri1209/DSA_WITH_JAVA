fa21539d3a1654edc508b16877e50e4b286d04fd
khushikri1209 <khushikumari12029@gmail.com>
2026-07-05 21:24:19 +0530

    Create J05LongestPrefix.java

---
+++ 03String/J05LongestPrefix.java
@@ -0,0 +1,30 @@
+public class J05LongestPrefix {
+
+    public static String longestCommonPrefix(String[] strs) {
+
+        if(strs.length==0) return "";
+
+        String prefix = strs[0];
+
+        for(int i=1;i<strs.length;i++){
+
+            while(strs[i].indexOf(prefix)!=0){
+                prefix = prefix.substring(0,prefix.length()-1);
+
+                if(prefix.isEmpty())
+                    return "";
+            }
+        }
+
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+
+        String[] arr = {"flower","flow","flight"};
+
+        System.out.println(longestCommonPrefix(arr));
+    }
+}
+    
+
